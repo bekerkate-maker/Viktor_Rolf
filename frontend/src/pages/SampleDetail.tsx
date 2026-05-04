f49d6347003c762dda1e1102e3b6b0912dcd46f9
Jouw Naam <jouw@emagit config --global user.name Kate Bekergit config --global user.email bekerkate@gmail.com>
2026-05-04 21:13:06 +0200

    Fine-tune navigation arrow horizontal alignment

--- frontend/src/pages/SampleDetail.tsx
+++ frontend/src/pages/SampleDetail.tsx
@@ -1048,7 +1048,7 @@ function SampleDetail() {
           <div style={{ 
             position: 'absolute', 
             top: 22, 
-            right: 16, 
+            right: 12, 
             display: 'flex', 
             alignItems: 'center', 
             gap: 8, 
